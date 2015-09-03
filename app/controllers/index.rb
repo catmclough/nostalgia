36ee26816929d4a01f08aeae2f01f9f5a420870c
Alex Green & Cat McLoughlin & Greg Ptasinski <pair+algreen88.catmclough.gptasinski@devbootcamp.com>
2015-09-03 15:06:34 -0500

    Add link to login with flickr

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,8 @@
 get "/" do
   erb :index
 end
+
+get "/login" do
+  digest = Digest::
+  https://www.flickr.com/services/auth/?api_key=04a77a5882d327512673de9a22bd2ced&perms=read&api_sig=7e17245dd9c7fec1090a76a4d4b0f0c2
+end
